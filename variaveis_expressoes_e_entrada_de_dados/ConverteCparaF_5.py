3e54910c5600e091606be13907864387fbbad7cd
vitorcoelho <vitorlimac2@gmail.com>
2021-01-05 10:41:36 -0300

    Lista 1 - questao 5

---
+++ variaveis_expressoes_e_entrada_de_dados/ConverteCparaF_5.py
@@ -0,0 +1,11 @@
+## ler temperatura em celsius
+
+celsius = float(input("Digite temperatura em graus Celsius: "))
+
+## cuidado com nome de variavel que tem grande chance errar a escrita
+
+fahrenheit = (9 * celsius)/5 + 32
+
+print("Temperatura em Celsius = %.2f" % celsius)
+print("Temperatura em Fahrenheit = %.2f" % fahrenheit)
+
